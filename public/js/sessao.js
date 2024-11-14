ea65ba73f7123e1606ad26ac1595a8a932ecfe07
ErickYuta <erick.miyagi@sptech.school>
2024-11-14 20:18:27 -0300

    Deixando a Dashboard interativa com as avaliações e tirando o simulador

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -2,11 +2,16 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
+    var generoLivro = sessionStorage.GENERO_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
-
+    var b_genero = document.getElementById("b_genero")
     if (email != null && nome != null) {
         b_usuario.innerHTML = nome;
+
+        if (b_genero && generoLivro != null) {
+            b_genero.innerHTML = generoLivro;
+        }
     } else {
         window.location = "../login.html";
     }
